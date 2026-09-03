e76e3171e0622fe3dcb77733730e95b825ceae34
Phillipe Mugnaini <phillipe841@gmail.com>
2026-09-03 20:13:23 -0300

    aula 03 - Parte 1

---
+++ 03 aula/parte 1/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node {
+
+    int valor;
+
+    Node* esquerdo;
+    Node* direito;
+};
+
+
+int main() {
+
+    Node* raiz = new Node{10, nullptr, nullptr};
+
+    raiz->esquerdo = new Node{5, nullptr, nullptr};
+    raiz->direito = new Node{15, nullptr, nullptr};
+
+    raiz->esquerdo->esquerdo = new Node{2, nullptr, nullptr};
+    raiz->esquerdo->direito = new Node{7, nullptr, nullptr};
+
+    raiz->direito->esquerdo = new Node{12, nullptr, nullptr};
+    raiz->direito->direito = new Node{20, nullptr, nullptr};
+
+
+    cout << "Arvore usando ponteiros:" << endl;
+
+    cout << raiz->valor << endl;
+
+    cout << raiz->esquerdo->valor << " ";
+    cout << raiz->direito->valor << endl;
+
+    cout << raiz->esquerdo->esquerdo->valor << " ";
+    cout << raiz->esquerdo->direito->valor << " ";
+    cout << raiz->direito->esquerdo->valor << " ";
+    cout << raiz->direito->direito->valor << endl;
+
+
+
+    // -------------------------
+    // PARTE 2 - VETOR
+    // -------------------------
+
+    vector<int> arvore = {10, 5, 15, 2, 7, 12, 20};
+
+
+    cout << endl;
+
+    cout << "Arvore usando vetor:" << endl;
+
+
+    for (int i = 0; i < arvore.size(); i++) {
+
+        cout << "Posicao " << i;
+        cout << " = " << arvore[i] << endl;
+    }
+
+
+    return 0;
+}
