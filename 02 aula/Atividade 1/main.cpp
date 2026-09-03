63fca42f5f8fa4ee53a06c76b3ab5f4bbdb3b040
Phillipe Mugnaini <phillipe841@gmail.com>
2026-09-03 19:46:56 -0300

    aula 02 - atividade 1

--- 02 aula/Atividade 1/main.cpp
+++ 02 aula/Atividade 1/main.cpp
@@ -20,3 +20,12 @@ int main() {
 
     return 0;
 }
+
+// 1. Estime o número de operações em função de n.
+// R:n^2 porque temos dois loops aninhados que percorrem de 0 a n.
+
+// 2. Classifique a complexidade assintótica.
+// R: O(n^2) - Complexidade quadrática.
+
+// Compare a contagem real (cont impresso na tela) para n = 10, 100, 1000.
+// R: Para n = 10, cont = 100; para n = 100, cont = 10000; para n = 1000, cont = 1000000. Isso confirma a estimativa de n^2.
